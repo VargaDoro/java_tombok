1f8d42ef97f5a7e9b279f5675717190eee31acca
VargaDoro <varga.dorothea.timea@gmail.com>
2025-03-19 10:17:06 +0100

    ---

--- TombHasznalata.java
+++ TombHasznalata.java
@@ -5,6 +5,8 @@ import java.util.Random;
 public class TombHasznalata {
     
     static final Random random = new Random();
+
+    private static String[] nevek;
     
     public static void main(String[] args) {
        int[] egeszek={10,20,30};
@@ -19,8 +21,21 @@ public class TombHasznalata {
        String s = "--- [0] = 40 (%d * valtoztat())".formatted(valtoztatasSzama);
        kiirTomb(s, egeszek);
        
+       nevek = new String[3];
+       nevek[0] = "Ede";
+       nevek[1] = "Éva";
+       nevek[2] = "Pál";
+       nevStatisztika(nevek);
     }
-
+    
+    private static void nevStatisztika(String[] tomb) {
+        for (int i = 0; i < tomb.length; i++) {
+            boolean van = tomb[i].contains("e");
+            System.out.printf("(%d) = %s\n", i+1, tomb[i]);
+        }
+        String ki = "hossza: (%d), 'e'-t tartalmaz: ".formatted(tomb[i].length());
+    }
+    
     private static void kiirTomb(String cim, int[] tomb) {
         System.out.println(cim);
         for (int i = 0; i < tomb.length; i++) {
